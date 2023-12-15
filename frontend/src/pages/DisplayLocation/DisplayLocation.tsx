5a4913ade46661a9fdc36636a8c03db0f84fc547
alexcourbon <courbon.alexandre49@gmail.com>
2023-12-15 10:42:51 +0100

    clean

--- frontend/src/pages/DisplayLocation/DisplayLocation.tsx
+++ frontend/src/pages/DisplayLocation/DisplayLocation.tsx
@@ -23,7 +23,7 @@ const DisplayLocationPage: React.FC<DisplayLocationPageProps> = () => {
         const dataLocation = await locationService.getLocationById(id || '');
         setLocation(dataLocation.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data', error);
       }
     };
 
